4332bb42fcae836120babef28efe323d5257051c
Mukilan R <mukil.aaa@gmail.com>
2020-05-10 21:54:06 +0530

    updated to HTTPS

---
+++ precache-manifest.e3b0947aff841ab0fd3e799f5a04174d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f917fb2802fc647acc305bfdb8f109af",
+    "url": "/product-comparison/index.html"
+  },
+  {
+    "revision": "ec7c2232f9a35a62f7d2",
+    "url": "/product-comparison/static/css/main.c8edf86d.chunk.css"
+  },
+  {
+    "revision": "5661294c6d3d739005f1",
+    "url": "/product-comparison/static/js/2.9956ead2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/product-comparison/static/js/2.9956ead2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec7c2232f9a35a62f7d2",
+    "url": "/product-comparison/static/js/main.8a986ff2.chunk.js"
+  },
+  {
+    "revision": "6b7f5a57fb2e9d155ba9",
+    "url": "/product-comparison/static/js/runtime-main.c9286e2c.js"
+  },
+  {
+    "revision": "6bb11a6dfcb287132fc7cb048e38904b",
+    "url": "/product-comparison/static/media/close.6bb11a6d.png"
+  }
+]);
